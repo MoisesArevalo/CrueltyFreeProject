8853ec204c0b0c2853ff7bed4f7e7e841a79659c
MoisesArevalo <arevalomoises00@gmail.com>
2021-04-30 15:59:19 -0500

    Funcionando el listar productos con uan variable estatica, sin consumir del API

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const scraper = require('./js/scraper');
 const apis = require('./js/apis');
 const bodyParser = require('body-parser');
 /// configuracion
-app.set('port',3000);
+app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -14,31 +14,40 @@ app.use(bodyParser.urlencoded({
 //app.use(express.urlencoded());
 app.use(express.json());
 // route
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   // res.sendFile(path.join(__dirname, 'index.html'));
   //console.log(scaper.test())
-  res.render('index', {status:'',img:'',business:[], news:[],business:['Cruelty Free'],images:['https://i.pinimg.com/originals/14/0d/2b/140d2b4f954537558d775a1551cc3c1f.jpg']});
+  const products = apis.getValuesProduct('');
+  res.render('index', {
+    status: '',
+    img: '',
+    business: [],
+    news: [],
+    business: ['Cruelty Free'],
+    images: ['https://i.pinimg.com/originals/14/0d/2b/140d2b4f954537558d775a1551cc3c1f.jpg'],
+    products: products
+  });
   //console.log(req.body);
 
 });
 // post
-const CRY ='https://giphy.com/embed/TRgyI2f0hRHBS';
+const CRY = 'https://giphy.com/embed/TRgyI2f0hRHBS';
 const HAPPY = 'https://giphy.com/embed/1136UBdSNn6Bu8';
 const WAIT = 'https://giphy.com/embed/PWfHC8ogZpWcE';
-app.post('/search', async(req,res)=>{
+app.post('/search', async (req, res) => {
   //console.log(test);
   // Status de la empresa
-  var gif =WAIT;
-  var status = await scraper.search(1,req.body.marca);
-  if(status[0].includes('Warning!')){
-    gif =CRY;
-  }else if (status[0].includes('NOT test')) {
+  var gif = WAIT;
+  var status = await scraper.search(1, req.body.marca);
+  if (status[0].includes('Warning!')) {
+    gif = CRY;
+  } else if (status[0].includes('NOT test')) {
     gif = HAPPY;
-  }else if (status[0].includes('found')) {
+  } else if (status[0].includes('found')) {
     gif = '';
   }
   /// Empresas cruelty Free
-  var business = await scraper.search(2,'Ecuador');
+  var business = await scraper.search(2, 'Ecuador');
   var image = [];
   //console.log(business);
   // for(var i=0; i<business.length;i++){
@@ -51,13 +60,23 @@ app.post('/search', async(req,res)=>{
   //   //console.log(elem);
   // } );
   //console.log(business);
-// console.log(image[0],business.length.toString());
+  // console.log(image[0],business.length.toString());
+  /// *** Productos relacionados
+  const products = apis.getValuesProduct('');
+  console.log
   ///// Noticias relacionadas
-  var news = await scraper.search(0,req.body.marca);
-  return res.render('index',{status: status[0], img:gif, business: business, news:news, business:business,images:['https://i.pinimg.com/originals/14/0d/2b/140d2b4f954537558d775a1551cc3c1f.jpg']});//, images : image
+  var news = await scraper.search(0, req.body.marca);
+  return res.render('index', {
+    status: status[0],
+    img: gif,
+    business: business,
+    news: news,
+    business: business,
+    images: ['https://i.pinimg.com/originals/14/0d/2b/140d2b4f954537558d775a1551cc3c1f.jpg']
+  }); //, images : image
 });
 // static files
-app.use(express.static(path.join(__dirname,'public')));
-app.listen(app.get('port'), ()=>{
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(app.get('port'), () => {
   console.log('Corriendo...', app.get('port'));
 });
